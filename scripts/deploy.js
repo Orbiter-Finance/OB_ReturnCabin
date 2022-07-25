859a40ebbd5c2083bc137ca09985d652260c2d2c
Eric <zu19890223@gmail.com>
2022-07-25 11:38:51 +0800

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+async function main() {
+  // npx hardhat run scripts/deploy.js --network <network-name>
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token = await ethers.getContractFactory("Test");
+  const token = await Token.deploy();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
