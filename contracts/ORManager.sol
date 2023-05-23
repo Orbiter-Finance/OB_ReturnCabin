9f14edd3683c35b88b3f06882175949b27efcbd4
linkdrone <fengwanqing2014@gmail.com>
2023-05-24 00:45:21 +0800

    chore: add ORManager configs

--- contracts/ORManager.sol
+++ contracts/ORManager.sol
@@ -8,7 +8,14 @@ import "./interface/IORProtocal.sol";
 import "./Multicall.sol";
 
 contract ORManager is IORManager, Ownable, Multicall {
-    mapping(uint => OperationsLib.ChainInfo) chains;
+    mapping(uint => OperationsLib.ChainInfo) private _chains;
+    address private _submitter;
+    uint64 private _protocolFee;
+    uint64 private _minChallengeRatio = 200;
+    uint64 private _challengeUserRatio;
+    uint64 private _feeManagerChallengeSecond;
+    uint64 private _feeManagerTakeOnChallengeSecond;
+    uint64 private _makerMaxLimit;
 
     constructor(address owner_) {
         _transferOwnership(owner_);
@@ -18,7 +25,7 @@ contract ORManager is IORManager, Ownable, Multicall {
     function registerChains(OperationsLib.ChainInfo[] calldata chains_) external onlyOwner {
         unchecked {
             for (uint i = 0; i < chains_.length; i++) {
-                chains[chains_[i].id] = chains_[i];
+                _chains[chains_[i].id] = chains_[i];
                 emit ChainInfoUpdated(chains_[i].id, chains_[i]);
             }
         }
@@ -28,13 +35,13 @@ contract ORManager is IORManager, Ownable, Multicall {
         unchecked {
             for (uint i = 0; i < spvs.length; i++) {
                 if (i < indexs.length) {
-                    chains[id].spvs[indexs[i]] = spvs[i];
+                    _chains[id].spvs[indexs[i]] = spvs[i];
                 } else {
-                    chains[id].spvs.push(spvs[i]);
+                    _chains[id].spvs.push(spvs[i]);
                 }
             }
         }
-        emit ChainInfoUpdated(id, chains[id]);
+        emit ChainInfoUpdated(id, _chains[id]);
     }
 
     function updateChainTokens(
@@ -45,16 +52,79 @@ contract ORManager is IORManager, Ownable, Multicall {
         unchecked {
             for (uint i = 0; i < tokens.length; i++) {
                 if (i < indexs.length) {
-                    chains[id].tokens[indexs[i]] = tokens[i];
+                    _chains[id].tokens[indexs[i]] = tokens[i];
                 } else {
-                    chains[id].tokens.push(tokens[i]);
+                    _chains[id].tokens.push(tokens[i]);
                 }
             }
         }
-        emit ChainInfoUpdated(id, chains[id]);
+        emit ChainInfoUpdated(id, _chains[id]);
     }
 
     function getChainInfo(uint id) external view returns (OperationsLib.ChainInfo memory chainInfo) {
-        chainInfo = chains[id];
+        chainInfo = _chains[id];
+    }
+
+    function submitter() external view returns (address) {
+        return _submitter;
+    }
+
+    function updateSubmitter(address submitter_) external onlyOwner {
+        _submitter = submitter_;
+        emit SubmitterFeeUpdated(_submitter);
+    }
+
+    function protocolFee() external view returns (uint64) {
+        return _protocolFee;
+    }
+
+    function updateProtocolFee(uint64 protocolFee_) external onlyOwner {
+        _protocolFee = protocolFee_;
+        emit ProtocolFeeUpdated(_protocolFee);
+    }
+
+    function minChallengeRatio() external view returns (uint64) {
+        return _minChallengeRatio;
+    }
+
+    function updateMinChallengeRatio(uint64 minChallengeRatio_) external onlyOwner {
+        _minChallengeRatio = minChallengeRatio_;
+        emit MinChallengeRatioUpdated(minChallengeRatio_);
+    }
+
+    function challengeUserRatio() external view returns (uint64) {
+        return _challengeUserRatio;
+    }
+
+    function updateChallengeUserRatio(uint64 challengeUserRatio_) external onlyOwner {
+        _challengeUserRatio = challengeUserRatio_;
+        emit ChallengeUserRatioUpdated(challengeUserRatio_);
+    }
+
+    function feeManagerChallengeSecond() external view returns (uint64) {
+        return _feeManagerChallengeSecond;
+    }
+
+    function updateFeeManagerChallengeSecond(uint64 feeManagerChallengeSecond_) external onlyOwner {
+        _feeManagerChallengeSecond = feeManagerChallengeSecond_;
+        emit FeeManagerChallengeSecondUpdated(feeManagerChallengeSecond_);
+    }
+
+    function feeManagerTakeOnChallengeSecond() external view returns (uint64) {
+        return _feeManagerTakeOnChallengeSecond;
+    }
+
+    function updateFeeManagerTakeOnChallengeSecond(uint64 feeManagerTakeOnChallengeSecond_) external onlyOwner {
+        _feeManagerTakeOnChallengeSecond = feeManagerTakeOnChallengeSecond_;
+        emit FeeManagerTakeOnChallengeSecondUpdated(feeManagerTakeOnChallengeSecond_);
+    }
+
+    function makerMaxLimit() external view returns (uint64) {
+        return _makerMaxLimit;
+    }
+
+    function updateMakerMaxLimit(uint64 makerMaxLimit_) external onlyOwner {
+        _makerMaxLimit = makerMaxLimit_;
+        emit MakerMaxLimitUpdated(makerMaxLimit_);
     }
 }
--- contracts/interface/IORManager.sol
+++ contracts/interface/IORManager.sol
@@ -5,6 +5,13 @@ import "../library/Operations.sol";
 
 interface IORManager {
     event ChainInfoUpdated(uint indexed id, OperationsLib.ChainInfo chainInfo);
+    event SubmitterFeeUpdated(address submitter);
+    event ProtocolFeeUpdated(uint64 protocolFee);
+    event MinChallengeRatioUpdated(uint64 minChallengeRatio);
+    event ChallengeUserRatioUpdated(uint64 challengeUserRatio);
+    event FeeManagerChallengeSecondUpdated(uint64 feeManagerChallengeSecond);
+    event FeeManagerTakeOnChallengeSecondUpdated(uint64 feeManagerTakeOnChallengeSecond);
+    event MakerMaxLimitUpdated(uint64 makerMaxLimit);
 
     function registerChains(OperationsLib.ChainInfo[] calldata chains_) external;
 
@@ -13,4 +20,32 @@ interface IORManager {
     function updateChainTokens(uint id, OperationsLib.TokenInfo[] calldata token, uint[] calldata indexs) external;
 
     function getChainInfo(uint id) external view returns (OperationsLib.ChainInfo memory chainInfo);
+
+    function submitter() external view returns (address);
+
+    function updateSubmitter(address submitter_) external;
+
+    function protocolFee() external view returns (uint64);
+
+    function updateProtocolFee(uint64 protocolFee_) external;
+
+    function minChallengeRatio() external view returns (uint64);
+
+    function updateMinChallengeRatio(uint64 minChallengeRatio_) external;
+
+    function challengeUserRatio() external view returns (uint64);
+
+    function updateChallengeUserRatio(uint64 challengeUserRatio_) external;
+
+    function feeManagerChallengeSecond() external view returns (uint64);
+
+    function updateFeeManagerChallengeSecond(uint64 feeManagerChallengeSecond_) external;
+
+    function feeManagerTakeOnChallengeSecond() external view returns (uint64);
+
+    function updateFeeManagerTakeOnChallengeSecond(uint64 feeManagerTakeOnChallengeSecond_) external;
+
+    function makerMaxLimit() external view returns (uint64);
+
+    function updateMakerMaxLimit(uint64 makerMaxLimit_) external;
 }
