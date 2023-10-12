afb9d73f28bd7bb4dd1ac1a98617faff43f5f08f
linkdrone <fengwanqing2014@gmail.com>
2023-10-12 19:22:34 +0800

    chore: add todo

--- contracts/example/OREventBinding.sol
+++ contracts/example/OREventBinding.sol
@@ -13,6 +13,7 @@ contract OREventBinding is IOREventBinding {
     function splitSecurityCode(uint securityCode) public pure returns (uint[] memory) {
         uint[] memory splits = new uint[](3);
 
+        // TODO: dealers occupy 2 places
         unchecked {
             splits[0] = (securityCode / 1000) % 10;
             splits[1] = (securityCode / 100) % 10;
